601d87b265cf41b5bd39d8986395c2940eb53d82
robynforrest <robynforrest@gmail.com>
2024-11-04 11:58:15 -0800

    Add Roxygen commentary

--- R/lib_summary.R
+++ R/lib_summary.R
@@ -1,3 +1,13 @@
+#' Provide Number of R Packages by Library
+#'
+#' Provide the number of R package by library in
+#'  a data.frame
+#'
+#' @return a data.frame of R packages by library
+#' @export
+#'
+#' @examples
+#' lib_summary()
 lib_summary <- function(){
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[,"LibPath"])
